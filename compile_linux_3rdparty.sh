c3fdb0f5371fc7f07d82014e91e4567e6652281b
houtianxin <houtianxin@syriusrobotics.com>
2024-04-25 17:05:41 +0800

    update package

--- compile_linux_3rdparty.sh
+++ compile_linux_3rdparty.sh
@@ -23,14 +23,14 @@ cd grpc && mkdir build && cd build && cmake .. \
   -DgRPC_BUILD_TESTS=OFF \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/grpc
 
-make -j && make install
+make -j4 && make install
 
 # dbus-cpp
 cd $PACKAGE_COMPILE_DIR
 
 git clone https://github.com/lib-cpp/dbus-cpp.git
 
-cd dbus-cpp && git checkout a0fc0e0 && git apply -p1 --ignore-space-change --whitespace=nowarn $GIT_PATCHE_DIR/dbus-cpp.patche
+cd dbus-cpp && git checkout a0fc0e0 && git apply -p1 --ignore-space-change --whitespace=nowarn $GIT_PATCHE_DIR/dbus-cpp.patch
 
 mkdir build && cd build && cmake .. \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/dbus-cpp
@@ -42,6 +42,98 @@ cd $PACKAGE_COMPILE_DIR
 
 git clone git://git.code.sf.net/p/dfu-util/dfu-util
 
-cd dfu-util && git checkout v0.9 && autoreconf -v -i && ./configure --prefix=${INSTALL_PREFIX_LINUX}/dfu-util # arm:--build=x86_64-linux-gnu --host=aarch64-linux-gnu
+cd dfu-util && git checkout v0.9
+ 
+autoreconf -v -i &&  ./configure --prefix=${INSTALL_PREFIX_LINUX}/dfu-util # arm:--build=x86_64-linux-gnu --host=aarch64-linux-gnu
+
+make -j && make install
+
+# json
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/open-source-parsers/jsoncpp
+
+cd jsoncpp && git checkout 1.8.4
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/jsoncpp \
+  -DJSONCPP_WITH_TESTS=OFF
+
+make -j && make install
+
+# fmt
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/fmtlib/fmt.git
+
+cd fmt && git checkout 8.0.1
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/fmt \
+  -DBUILD_SHARED_LIBS=ON \
+  -DFMT_TEST=OFF
+
+make -j && make install
+
+# yaml-cpp
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/SiriusVoyager/yaml-cpp.git
+
+cd yaml-cpp && git checkout yaml-cpp-0.6.2
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/yaml-cpp \
+  -DYAML_CPP_BUILD_TESTS=OFF \
+  -DCMAKE_CXX_FLAGS=-fPIC
+
+make -j && make install
+
+# gflag
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/gflags/gflags.git
+
+cd gflags && git checkout v2.2.2
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/gflags \
+  -DCMAKE_CXX_FLAGS=-fPIC
+
+make -j && make install
+
+# glog
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/google/glog.git
+
+cd glog && git checkout v0.5.0
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/glog
+
+make -j && make install
+
+# libcanard
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/UAVCAN/libcanard
+
+cd libcanard && git checkout 54a004bd && git apply -p1 --ignore-space-change --whitespace=nowarn $GIT_PATCHE_DIR/libcanard.patch
+
+mkdir build && cd build && cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_LINUX}/libcanard \
+  -DCMAKE_CXX_FLAGS=-fPIC
+
+make -j && make install
+
+# libusb
+cd $PACKAGE_COMPILE_DIR
+
+git clone https://github.com/libusb/libusb.git
+
+cd libusb && git checkout v1.0.26
+
+autoreconf -v -i &&  ./configure --enable-udev=no --with-pic --prefix=${INSTALL_PREFIX_LINUX}/libusb 
 
 make -j && make install
