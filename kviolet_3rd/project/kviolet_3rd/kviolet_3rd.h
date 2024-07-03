7b1a864bdefb7779421f1fdc5f2e1c48c80be9eb
arkviolet <h18209294659@aliyun.com>
2024-07-03 09:08:20 +0800

    add http client

--- kviolet_3rd/project/kviolet_3rd/kviolet_3rd.h
+++ kviolet_3rd/project/kviolet_3rd/kviolet_3rd.h
@@ -1,6 +1,7 @@
 #ifndef __KVIOLET__KVIOLET__TOOLKIT__3RD__H__
 #define __KVIOLET__KVIOLET__TOOLKIT__3RD__H__
 
+#include "http_client/http_client.h"
 #include "speaker_gst/speaker_gst.h"
 #include "speaker_pulseaudio/speaker_pulseaudio.h"
 
