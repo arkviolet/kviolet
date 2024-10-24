68c8c83ece725312f0020ce83796c1f67c13ab1e
kviolet <h18309294659@aliyun.com>
2024-10-24 18:07:14 +0800

    cross h3 gdb

--- compile_cross_3rdparty.sh
+++ compile_cross_3rdparty.sh
@@ -55,6 +55,7 @@ cd $PACKAGE_COMPILE_DIR/grpc/cd build_h3_r258
 make -j8 && make install
 
 # gdb
+# Rockchip rk3568
 source /opt/workspace/Rockchip/rk3568/aarch64-linux-gcc-v12.3/environment-setup
 export "CONFIGURE_FLAGS=--target=aarch64-buildroot-linux-gnu --host=aarch64-buildroot-linux-gnu --build=x86_64-pc-linux-gnu"
 
@@ -133,5 +134,97 @@ tar -xvf gdb-8.1.tar.gz && cd gdb-8.1/
 			--localstatedir=/home/${USER}/software/kviolet_cross_3rdparty/gun/var
 
 sed -i '179,181d' gdb/nat/linux-ptrace.h
+
+make -j8 && make install
+
+# gdb
+# Allwinner h3_r258
+export PATH=$PATH:/opt/workspace/Allwinner/h3_r258/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf/bin/
+export "CONFIGURE_FLAGS=--host=arm-linux-gnueabihf --build=x86_64-pc-linux-gnu"
+export CC="arm-linux-gnueabihf-gcc"
+
+cd $PACKAGE_COMPILE_DIR
+
+wget http://mirrors.ustc.edu.cn/gnu/gmp/gmp-6.0.0a.tar.xz
+
+tar -xvf gmp-6.0.0a.tar.xz && cd gmp-6.0.0
+
+./configure	$CONFIGURE_FLAGS \
+			--prefix=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--enable-cxx \
+			--enable-fft
+
+make -j8 && make install
+
+rm /home/${USER}/software/kviolet_cross_3rdparty/gun/lib/lib*.la
+
+cd $PACKAGE_COMPILE_DIR
+
+wget http://mirrors.ustc.edu.cn/gnu/mpfr/mpfr-3.1.3.tar.xz
+
+tar -xvf mpfr-3.1.3.tar.xz && cd mpfr-3.1.3/
+
+./configure	$CONFIGURE_FLAGS \
+			--prefix=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-gmp=/home/${USER}/software/kviolet_cross_3rdparty/gun
+
+make -j8 && make install
+
+rm /home/${USER}/software/kviolet_cross_3rdparty/gun/lib/lib*.la
+
+cd $PACKAGE_COMPILE_DIR
+
+wget http://mirrors.ustc.edu.cn/gnu/mpc/mpc-1.0.3.tar.gz
+
+tar -xvf mpc-1.0.3.tar.gz && cd mpc-1.0.3/
+
+./configure	$CONFIGURE_FLAGS \
+			--prefix=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-gmp=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-mpfr=/home/${USER}/software/kviolet_cross_3rdparty/gun
+
+make -j8 && make install
+
+cd $PACKAGE_COMPILE_DIR
+
+rm /home/${USER}/software/kviolet_cross_3rdparty/gun/lib/lib*.la
+
+wget http://mirrors.ustc.edu.cn/gnu/gdb/gdb-8.0.tar.gz
+
+tar -xvf gdb-8.0.tar.gz && cd gdb-8.0/
+
+./configure $CONFIGURE_FLAGS \
+			--prefix=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-gmp=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-mpfr=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--with-mpc=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--program-suffix=8.0 \
+			--with-gnu-ld \
+			--enable-plugins \
+			--enable-tui \
+			--with-pkgversion=Linaro_GDB-2017.10 \
+			--disable-gas \
+			--disable-binutils \
+			--disable-ld \
+			--disable-gold \
+			--disable-gprof \
+			--without-curses \
+			--enable-tui=no	\
+			--exec-prefix=/home/${USER}/software/kviolet_cross_3rdparty/gun \
+			--sysconfdir=/home/${USER}/software/kviolet_cross_3rdparty/gun/etc \
+			--localstatedir=/home/${USER}/software/kviolet_cross_3rdparty/gun/var
+
+make -j8 && make install
+
+#ncurses
+cd $PACKAGE_COMPILE_DIR
+
+wget  https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.2.tar.gz
+
+tar xvf ncurses-6.2.tar.gz && cd ncurses-6.2/
+
+./configure $CONFIGURE_FLAGS \
+            --with-shared \
+		        --prefix=/home/${USER}/software/kviolet_cross_3rdparty/ncurses
 
 make -j8 && make install
